4a9bff07b35d5ce3a2948c935173e64c9f9852a6
Juunibyou <danijunn@gmail.com>
2025-11-03 18:36:20 -0600

    update

--- app/src/main/java/org/example/ProcessText.java
+++ app/src/main/java/org/example/ProcessText.java
@@ -1,4 +1,6 @@
 package org.example;
+
+import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
@@ -9,5 +11,27 @@ public class ProcessText {
     ));
 
     public static List<String> readWords(String filename) throws IOException {
+        List<String> words = new ArrayList<>();
+
+        try (Scanner scanner = new Scanner(new File(filename))){
+            while(scanner.hasNext()){
+                String word = scanner.next();
+                String cleanWord = "";
+
+                for (int i = 0; i < word.length(); i++){
+                    char c = word.charAt(i);
+
+                    if(Character.isLetterOrDigit(c) || c == '-'){
+                        cleanWord += Character.toLowerCase(c);
+                    }
+                }
+
+                if(!cleanWord.isEmpty() && !StopWords.contains(cleanWord)){
+                    words.add(cleanWord);
+                }
+            }
+        }
+    
+        return words;
     }
 }
